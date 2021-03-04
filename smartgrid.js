a3d678e5ec0d8e34383248e6b2c980c6f93bb065
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-03-04 08:57:39 +0300

    initial

---
+++ smartgrid.js
@@ -0,0 +1,23 @@
+module.exports = {
+  columns: 12,
+    offset: '30px', //columns
+    container: {
+      maxWidth: '1170px',
+        fields: '20px' // container
+      },
+      breakPoints: {
+        // xl: {
+        //   width: '1200px'
+        // },
+        lg: {
+         width: '992px'
+        },
+        md: {
+         width: '768px'
+        },
+        sm: {
+         width: '576px'
+        }
+      },
+     // detailedCalc: true
+   };
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//= ../../node_modules/jquery-form-styler/dist/jquery.formstyler.min.js
+
+$(function(){
+	
+	$('select').styler();
+
+});
+
