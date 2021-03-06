d582c2a31f624aa7d8b32186ec2a50a07238b6f3
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-03-06 20:22:00 +0300

    all done

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,25 @@
 //= ../../node_modules/jquery-form-styler/dist/jquery.formstyler.min.js
+//= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
 //= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
 
 $(function(){
 	
-	$('select').styler();
+	$('input, select').styler({
+		onSelectOpened: function() {
+			// к открытому селекту добавляется красная обводка
+			// $(this).css('outline', '3px solid red');
+		},
+		locale: 'ru',
+		locales: {
+			'ru': {
+				filePlaceholder: 'Перетащите сюда фотографии или',
+				fileBrowse: 'выберите файлы',
+				fileNumber: 'Выбрано: %s',
+			}
+		},
+	});
+
+	$('input[type="tel"]').mask("+7 (999) 999-9999");
 
 	//= components/slider.js
 
