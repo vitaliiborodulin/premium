4fba4903353c3dcf832a222ca40a9e94d47de40e
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-03-17 09:41:52 +0300

    updates

--- smartgrid.js
+++ smartgrid.js
@@ -16,7 +16,8 @@ module.exports = {
          width: '768px'
         },
         sm: {
-         width: '576px'
+         width: '576px',
+				 fields: '10px'
         }
       },
      // detailedCalc: true
